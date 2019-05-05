898695341c3011643cfb34ba38f2a6c704a8e934
Daisy Crego <daisycrego@gmail.com>
2019-05-05 16:34:27 -0400

    Push

--- parallel_hashtable_data.sh
+++ parallel_hashtable_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-touch $1
+touch $2
 
-echo "value,   keys inserted, insertion time (s), keys lost during retrieval, retrieval time (s)" >> $1
+echo "value, keys inserted, insertion time (s), keys lost during retrieval, retrieval time (s)" > $1
 
-for value in {1..50}
+for value in {1..1000}
 do
-  output=`./parallel_semaphore_forcsv $value`
-  echo "$value, $output" >> $1
+  output=`./parallel_$1_forcsv $value`
+  echo "$value, $output" >> $2
 done
