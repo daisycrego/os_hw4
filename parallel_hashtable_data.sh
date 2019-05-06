34e7a97e4e6cff15dcee7db02ba439e61310f343
Daisy Crego <daisycrego@gmail.com>
2019-05-05 22:15:25 -0400

    Prepping semaphore version for VM.

--- parallel_hashtable_data.sh
+++ parallel_hashtable_data.sh
@@ -2,10 +2,11 @@
 
 touch $2
 
-echo "value, keys inserted, insertion time (s), keys lost during retrieval, retrieval time (s)" > $1
+echo "value, keys inserted, insertion time (s), keys retrieved, retrieval time (s)" > $2
 
 for value in {1..1000}
 do
   output=`./parallel_$1_forcsv $value`
   echo "$value, $output" >> $2
+  echo "$value..."
 done
